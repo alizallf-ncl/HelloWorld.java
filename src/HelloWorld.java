fc58625a207cfbdb964d7cd8956871dda8026bd9
Aliza Lim <alizalim@newhuaweiapstudent-10-58-196-227.ncl.ac.uk>
2022-09-30 10:52:51 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,95 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        //practical-1.1
+        System.out.println("Aliza");
+        System.out.println("CSC8011 Introduction to Software Engineering");
+        System.out.println("Hello world!");
+
+
+        //practical-1.2
+        int addition = 100 + 50;
+        System.out.println(addition);
+
+
+        //practical-1.2.1
+        int x = 100;
+        int y = 50;
+
+        int z = x + y;
+
+        System.out.println(z);
+
+        //practical-1.2.2
+
+        int a = -22 + 3*2;
+        int b = (9/2) % 9;
+       long c = 2_147_483_646 + -15/2;
+        int d = (-2_147_483_646 + -1000/2) *10;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        // Practical-1.3: Addition
+
+        int addX = 250;
+        int addY = 72;
+
+        int additionXY = addX + addY;
+
+        System.out.println(additionXY);
+
+        //Practical-1.3: Multiplication
+
+        int multX = 250;
+        int multY = 72;
+
+        int multiplicationXY = multX * multY;
+        System.out.println(multiplicationXY);
+
+
+        // Practical-1.3: Subtraction
+
+        int subX = 250;
+        int subY = 72;
+
+        int subtractionXY = subX - subY;
+
+        System.out.println(subtractionXY);
+
+        // Practical-1.3: Division
+
+        int divX = 250;
+        int divY = 72;
+
+        int divisionXY = divX / divY;
+
+        System.out.println(divisionXY);
+
+        // Practical-1.3: Remainder
+
+        int remX = 250;
+        int remY = 72;
+
+        int remainderXY = 250 % 72;
+
+        System.out.println(remainderXY);
+
+        //Practical-1.4
+
+      String fancyPrintLine1 = "   J    a   v     v  a";
+      String fancyPrintLine2 = "   J   a a   v   v  a a";
+      String fancyPrintLine3 = "J  J  aaaaa   V V  aaaaa";
+      String fancyPrintLine4 = " JJ  a     a   V  a     a";
+
+
+        System.out.println(fancyPrintLine1);
+        System.out.println(fancyPrintLine2);
+        System.out.println(fancyPrintLine3);
+        System.out.println(fancyPrintLine4);
+
+
+    }
+}
